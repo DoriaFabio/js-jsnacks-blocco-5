bca3bdcc64cd07041e0f5344eb665bff57307ead
DoriaFabio <doriafabio2001@gmail.com>
2024-11-15 13:55:43 +0100

    do 3 snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,9 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const numbersPiu = numbers.map((numero) => {
+    return numero + 1;
+});
+
+console.log(numbersPiu);
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
